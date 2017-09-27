340eaac00ae79214673f4d186984f80b6eda0cfb
Jose Silva <jose.silva@qpercom.ie>
2017-09-27 12:35:31 +0100

    local notification

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import { ErrorHandler, NgModule } from '@angular/core';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [
+    LocalNotifications
+  ]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private localNote: LocalNotifications
+  ) {
 
+  }
+
+  public setLocalNotification(): void{
+    console.log('here');
+    let notification = 
+      {
+        text: 'blablabla',
+        at: new Date(new Date().getTime() + 20000)
+      };
+    console.log('notification fired', notification);
+    this.localNote.schedule(notification)
   }
 
 }
